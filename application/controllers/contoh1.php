fc600eaa6be9f9131d96fd5467c29bacaebd754f
arifinyanuar <pipinasomething1@gmail.com>
2023-10-17 13:31:40 +0700

    tugas p2

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,1 +1,12 @@
-<?php class Contoh1 extends CI_Controller { public function index() { echo "<h1>Perkenalkan</h1>"; echo"Nama saya Imam Nawawi Saya tingga di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis"; } }
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Arifin Yanuar Saputra
+ Saya tinggal di daerah Bekasi
+ hobi yang saya sukai adalah
+ Photography";
+ }
+}
